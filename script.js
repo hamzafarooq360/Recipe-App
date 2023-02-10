a24c27ff250de7a5fecf8e9ba9a3085f7bb845a4
Hamza Farooq <hamzafarooq360@gmail.com>
2023-02-10 10:41:02 -0500

    all recipes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Initial References
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
@@ -11,57 +10,60 @@ searchBtn.addEventListener("click", () => {
     fetch(url + userInp)
       .then((response) => response.json())
       .then((data) => {
-        let myMeal = data.meals[0];
-        console.log(myMeal);
-        console.log(myMeal.strMealThumb);
-        console.log(myMeal.strMeal);
-        console.log(myMeal.strArea);
-        console.log(myMeal.strInstructions);
-        let count = 1;
-        let ingredients = [];
-        for (let i in myMeal) {
-          let ingredient = "";
-          let measure = "";
-          if (i.startsWith("strIngredient") && myMeal[i]) {
-            ingredient = myMeal[i];
-            measure = myMeal[`strMeasure` + count];
-            count += 1;
-            ingredients.push(`${measure} ${ingredient}`);
+        let meals = data.meals;
+        result.innerHTML = "";
+        meals.forEach((meal) => {
+          let count = 1;
+          let ingredients = [];
+          for (let i in meal) {
+            let ingredient = "";
+            let measure = "";
+            if (i.startsWith("strIngredient") && meal[i]) {
+              ingredient = meal[i];
+              measure = meal[`strMeasure` + count];
+              count += 1;
+              ingredients.push(`${measure} ${ingredient}`);
+            }
           }
-        }
-        console.log(ingredients);
 
-        result.innerHTML = `
-    <img src=${myMeal.strMealThumb}>
-    <div class="details">
-        <h2>${myMeal.strMeal}</h2>
-        <h4>${myMeal.strArea}</h4>
-    </div>
-    <div id="ingredient-con"></div>
-    <div id="recipe">
-        <button id="hide-recipe">X</button>
-        <pre id="instructions">${myMeal.strInstructions}</pre>
-    </div>
-    <button id="show-recipe">View Recipe</button>
-    `;
-        let ingredientCon = document.getElementById("ingredient-con");
-        let parent = document.createElement("ul");
-        let recipe = document.getElementById("recipe");
-        let hideRecipe = document.getElementById("hide-recipe");
-        let showRecipe = document.getElementById("show-recipe");
+          let mealDiv = document.createElement("div");
+          mealDiv.classList.add("meal");
+          mealDiv.innerHTML = `<div>
+            <img src=${meal.strMealThumb}>
+            <div class="details">
+                <h2>${meal.strMeal}</h2>
+                <h4>${meal.strArea}</h4>
+            </div>
+            <div class="ingredient-con"></div>
+            <div class="recipe">
+                <button class="hide-recipe">X</button>
+                <p class="instructions" style="display:block;">${meal.strInstructions}</p>
+            </div>
+            <button class="show-recipe">Show Recipe </button>
+            </div>
+          `;
 
-        ingredients.forEach((i) => {
-          let child = document.createElement("li");
-          child.innerText = i;
-          parent.appendChild(child);
-          ingredientCon.appendChild(parent);
-        });
+          let ingredientCon = mealDiv.querySelector(".ingredient-con");
+          let parent = document.createElement("ul");
+          let recipe = mealDiv.querySelector(".recipe");
+          let hideRecipe = mealDiv.querySelector(".hide-recipe");
+          let showRecipe = mealDiv.querySelector(".show-recipe");
 
-        hideRecipe.addEventListener("click", () => {
-          recipe.style.display = "none";
-        });
-        showRecipe.addEventListener("click", () => {
-          recipe.style.display = "block";
+          ingredients.forEach((i) => {
+            let child = document.createElement("li");
+            child.innerText = i;
+            parent.appendChild(child);
+            ingredientCon.appendChild(parent);
+          });
+
+          hideRecipe.addEventListener("click", () => {
+            recipe.style.display = "none";
+          });
+          showRecipe.addEventListener("click", () => {
+            recipe.style.display = "block";
+          });
+
+          result.appendChild(mealDiv);
         });
       })
       .catch(() => {
@@ -69,3 +71,4 @@ searchBtn.addEventListener("click", () => {
       });
   }
 });
+
